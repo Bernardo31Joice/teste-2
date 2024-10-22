8d7dc38ab0c13abb2af94af1d133df81b90588c8
Bernardo31Joice <bernardo.joice@escola.pr.gov.br>
2024-10-22 00:07:55 -0300

    gtrhg

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 let randomNumber =
 Math.floor(Math.random()*10)+1;
 let attempts = 0;
+
 function checkGuess (){
-const userGuess = parseInt(document.getElementById('guess').value);
-attempts++;
-if(userGuess === randomNumber) {
+    const userGuess = parseInt(document.getElementById('guess').value);
+    attempts++;
+
+    if(userGuess === randomNumber) {
+
 document.getElementById('message').innerText = 'Parabéns, você acertou!';
-} else if (userGuess < randomNumber)
+    } else if (userGuess < randomNumber)
 {
 document.getElementById('message').innerText = 'Muito baixo! Tente novamente.';
-} else {
+    } else {
 
 document.getElementById('message').innerText = 'Muito alto! Tente novamente.';
-
-}
+    }
 }
